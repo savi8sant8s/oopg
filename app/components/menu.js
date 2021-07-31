9e21063bf00eab6c81bb3ee024cdc3f949000e07
Savio Santos <saviosa08@gmail.com>
2021-07-31 10:13:28 -0300

    :sparkles: Criado estrutura do frontend.

---
+++ app/components/menu.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+export default class Menu extends Component {
+
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-light bg-light">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="/">
+                            OOPG
+                        </a>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
+}
---
+++ app/pages/index.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+export default class Home extends Component {
+
+  render(){
+    return (
+      <div>
+       Iremos começar
+      </div>
+    )
+  }
+}
