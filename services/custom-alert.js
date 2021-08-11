2cf739bb1f11146c0e2d8af1e4933bc3bf266853
Savio Santos <saviosa08@gmail.com>
2021-08-11 13:18:12 -0300

    :recycle: Alterado tempo de alerta.

--- services/custom-alert.js
+++ services/custom-alert.js
@@ -1,6 +1,6 @@
 import Swal from 'sweetalert2';
 
-export default function presentAlert(title, text, icon = "info", timer = 2000){
+export default function presentAlert(title, text, icon = "info", timer = 2500){
     Swal.fire({
         icon: icon,
         title: title,
